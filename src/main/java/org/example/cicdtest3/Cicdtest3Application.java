d7515bb93eeda9026f148296b71e46869faa539d
waycsj <waycsj@gmail.com>
2024-11-27 11:49:48 +0900

    first commit

---
+++ src/main/java/org/example/cicdtest3/Cicdtest3Application.java
@@ -0,0 +1,13 @@
+package org.example.cicdtest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicdtest3Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Cicdtest3Application.class, args);
+  }
+
+}
