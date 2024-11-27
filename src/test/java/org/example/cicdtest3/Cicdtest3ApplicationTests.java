2c3752e0a48938ff67ed49170ca6d10ca59acaa8
waycsj <waycsj@gmail.com>
2024-11-27 15:33:13 +0900

    sql commit3

--- src/test/java/org/example/cicdtest3/Cicdtest3ApplicationTests.java
+++ src/test/java/org/example/cicdtest3/Cicdtest3ApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.cicdtest3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Cicdtest3ApplicationTests {
 
   @Test
--- src/test/java/org/example/cicdtest3/controller/MainControllerTest.java
+++ src/test/java/org/example/cicdtest3/controller/MainControllerTest.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+//@SpringBootTest
 class MainControllerTest {
 @Test
   void main() {
